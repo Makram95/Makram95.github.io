c51cdbd87da3f9448131c14a523ee6c9a94a11d3
Makram95 <marc.kramer95@icloud.com>
2021-05-23 21:49:50 +0200

    added: new section

---
+++ Content/apps/first-app.md
@@ -0,0 +1,8 @@
+---
+date: 2021-05-23 21:06
+description: A description of my first app.
+tags: first, article
+---
+# My first app
+
+My first apps's text.
--- Sources/Makram95GithubIo/main.swift
+++ Sources/Makram95GithubIo/main.swift
@@ -7,6 +7,7 @@ struct Makram95GithubIo: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
         case posts
+        case apps
     }
 
     struct ItemMetadata: WebsiteItemMetadata {
